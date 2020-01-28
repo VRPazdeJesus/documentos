2468639db50e1c89a41c88e72d8db7c9cf0f400a
Víctor Romário <victor.romario.seriado@gmail.com>
2020-01-28 20:49:08 -0300

    Busca da pasta pelo CPF no Firebase storage

---
+++ js/initFirebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCKm1FnIAIcslSoYd_2etep8F2sfx0foUE",
+    authDomain: "documentos-16052.firebaseapp.com",
+    databaseURL: "https://documentos-16052.firebaseio.com",
+    projectId: "documentos-16052",
+    storageBucket: "documentos-16052.appspot.com",
+    messagingSenderId: "1095355022862",
+    appId: "1:1095355022862:web:b03a9f3a5598393fd2065f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
